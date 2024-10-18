3a1895df522e6ca718fa77ffa095a9b9dc6d7828
abhimanyu-stream <abhimanyu.intelligence@gmail.com>
2024-10-19 05:20:09 +0530

    programs updated

--- src/main/java/com/java17/utilityprograms/HowToStoreEnumValueAsStringInDB.java
+++ src/main/java/com/java17/utilityprograms/HowToStoreEnumValueAsStringInDB.java
@@ -1,11 +1,19 @@
 package com.java17.utilityprograms;
 
 
+import org.springframework.boot.SpringApplication;
+
 public class HowToStoreEnumValueAsStringInDB {
 
     public static void main(String[] args) {
+        SpringApplication.run(JavaProgramsApplication.class, args);
 
         System.out.println(ProductSubCategory.SMARTPHONES);
+        System.out.println(ProductSubCategory.CLOTHINGTOPWAER);
+        System.out.println(ProductSubCategory.CLOTHINGUNDERWEAR);
+        System.out.println(ProductSubCategory.CLOTHINGBOTTOMWEAR);
+        System.out.println(ProductSubCategory.SHOEFORMAL);
+        System.out.println(ProductSubCategory.SHOESPORTS);
         System.out.println(ProductCategory.GROCERY.name());
 
     }
@@ -13,14 +21,20 @@ public class HowToStoreEnumValueAsStringInDB {
 enum ProductSubCategory {
 
     //Note remove arguments if some mismatch info store in db
-    SMARTPHONES(ProductCategory.ELECTRONICS.name()),
+    SMARTPHONES(ProductCategory.ELECTRONICS.name()+ "->" +"SMARTPHONES"),
     LAPTOPS(ProductCategory.ELECTRONICS.name()),
     DESKTOP(ProductCategory.ELECTRONICS.name()),
     TABLET(ProductCategory.ELECTRONICS.name()),
 
-    CLOTHING(ProductCategory.FASHION.name()),
+    CLOTHINGTOPWAER(ProductCategory.FASHION.name()+"->"+"CLOTHING"+"->"+"TOPWAER"),
+    CLOTHINGUNDERWEAR(ProductCategory.FASHION.name()+"->"+"CLOTHING"+"->"+"UNDERWEAR"),
+    CLOTHINGBOTTOMWEAR(ProductCategory.FASHION.name()+"->"+"CLOTHING"+"->"+"BOTTOMWEAR"),
+
+
     FOOTWEAR(ProductCategory.FASHION.name()),
-    SUNGLASS(ProductCategory.FASHION.name());
+    SUNGLASS(ProductCategory.FASHION.name()),
+    SHOEFORMAL(ProductCategory.FASHION.name()+"->"+"NON-CLOTHING"+"->"+"SHOEFORMAL"),
+    SHOESPORTS(ProductCategory.FASHION.name()+"->"+"NON-CLOTHING"+"->"+"SHOESPORTS");
 
     public String getProductSubCategory() {
         return productSubCategory;
@@ -34,6 +48,11 @@ enum ProductSubCategory {
     }
     public String toString() {
         return productSubCategory;
+    }
+    private String parentCategory;
+
+    public String getParentCategory() {
+        return getProductSubCategory();
     }
 
 
